f76758c232d24fcced33e7a45f71fbc41a5cdb8c
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2024-01-03 17:28:29 +0600

    vercel conf

--- meta_chef/settings.py
+++ meta_chef/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-bw%giwtim^_u9lgea!-apiy77(@@u(py7^!dhuy4m8q!e((38-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
 
 # Application definition
