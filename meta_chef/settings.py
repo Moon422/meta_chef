8026ea71393660315bd92fda3b0f7e07e859a019
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2024-01-03 19:50:18 +0600

    renamed static root

--- meta_chef/settings.py
+++ meta_chef/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "www" / "static",
 ]
-STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
